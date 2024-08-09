d7bce00f88c64744d0a34474596a70a0313b2c25
Kal <hiratelli.0@gmail.com>
2024-08-09 12:32:37 -0300

    ajustes finais

--- src/main/java/com/warehouse/warehouse/database/DatabaseConnector.java
+++ src/main/java/com/warehouse/warehouse/database/DatabaseConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/warehouse?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3307/warehouse?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "admin";
 
